5e2a3961b6bb53f232dce65eba17e3e9a80352a8
Szabolcs Hubai <szab.hu@gmail.com>
2023-08-25 22:34:33 +0200

    v2.0

--- overlay/build.gradle.kts
+++ overlay/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         namespace = applicationId
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
